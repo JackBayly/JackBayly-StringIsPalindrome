134c3def5b6762071366d4dae5b20e959f36f237
Jack O'Hara <jackohara96@gmail.com>
2023-05-01 12:55:12 +0000

    Palindrome practice

--- src/main/java/Palindrome.java
+++ src/main/java/Palindrome.java
@@ -11,7 +11,14 @@ public class Palindrome {
      * @param str A String.
      * @return true if str is a palindrome, false otherwise.
      */
-    public boolean pal(String str){
+    public boolean pal(String str){ 
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse = reverse + str.charAt(i);
+        }
+        if (str.equals(reverse)) {
+            return true;
+        }
         return false;
     }
 }
